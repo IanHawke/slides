2352354a7ab8685fa35728e3bb427ffb78a81730
Ian Hawke <I.Hawke@soton.ac.uk>
2023-06-20 09:31:30 +0100

    Using new pointer library

---
+++ et-2023/reveal_defaults.js
@@ -0,0 +1,13 @@
+Reveal.initialize({
+  width: 1536,
+  height: 960,
+  margin: 0.04,
+  controls: false,
+  progress: true,
+  center: true,
+  hash: true,
+  transition: 'none',
+  pdfSeparateFragments: true,
+  // Learn about plugins: https://revealjs.com/plugins/
+  plugins: [ RevealZoom, RevealNotes, RevealSearch, RevealMarkdown, RevealHighlight, RevealMath.KaTeX, RevealPointer],
+});
